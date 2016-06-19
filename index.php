9bb875240e9147cb9aa9d2cfffec10e486efdded
Rafael Augusto <rafa.2098@gmail.com>
2016-06-19 16:26:41 -0300

    Alterações

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php
 date_default_timezone_set('America/Sao_Paulo');
 
+// Horario de Abertura
 $abertura 	= '07:00';
+
+// Horario de Fechamento
 $fechamento = '15:53';
+
+// Hora atual
 $horarioAtual = date("H:i");
 
 
-
+// Verifica se a hora atual e maior ou igual ao da abertura e se a hora atual e menor que o fechamento
 if($horarioAtual >= strtotime($abertura) || $horarioAtual < $fechamento){
 	echo "Estabelecimento Aberto";
 }else{
 	echo "Estabeleccimento Fechado";
 }
+
+
+// Caso queira pegar do banco apenas os estabelecimentos que estejam abertos utilize esse SQL
+
+// SELECT * FROM `estabelecimentos` WHERE `abertura` <= NOW() AND `fechamento` > NOW()
